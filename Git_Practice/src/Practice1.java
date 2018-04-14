47d8059bd7ebe82b871cb60c7fad0d46db51fbcb
CAU HHS <CAU HHS@10.210.56.98>
2018-04-14 17:03:35 +0900

    commit...

--- Git_Practice/src/Practice1.java
+++ Git_Practice/src/Practice1.java
@@ -1,4 +1,18 @@
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
 
 public class Practice1 {
 	public static class DB{
@@ -8,31 +22,69 @@ public class Practice1 {
 			con = null;
 		}
 		
-		void open(String id, String pw) {
-			/* Fill */
+		void open() throws SQLException, IOException {
+			con = DriverManager.getConnection
+					("jdbc:mysql://35.201.230.135/hhs?useSSL=false", "javateam", "boradori1");
 		}
 		
-		String[] list() {
+		String[] list() throws SQLException {
 			String item[] = null;
-			/* Fill */
+			Statement  st = con.createStatement();
+	        ResultSet rs;
+
+			String sql = "SELECT * FROM hhs.GitPractice";
+
+	        rs = st.executeQuery(sql);
+	        ResultSetMetaData rsmd = rs.getMetaData();
+	        
+	        // 행 갯수 세기
+	        rs.last();
+	        int NumRow = rs.getRow();
+	        System.out.println("Number of Rows : " +NumRow);
+	        
+	        item = new String [NumRow];
+	        
+	        // 열 갯수 세기
+	        int numberOfColumns=rsmd.getColumnCount();
+	        System.out.println("Number of Columns : " + numberOfColumns);
+	        
+	        //행 처음으로 돌아가기
+	        rs.beforeFirst();
+	        for (int i = 0 ; rs.next() ; i ++) {
+	        	item[i] = rs.getString(1);
+	        	for (int j = 2 ; j <= numberOfColumns ; j ++) {
+			        item[i] = item[i] + "\t" + rs.getString(j);
+	        	}
+	        }
+	        
 			return item;
 		}
 		
-		boolean find(String item) {
-			/* Fill */
-			return false;
+		boolean find(String item) throws SQLException {
+			Statement  st = con.createStatement();
+	        ResultSet rs;
+
+			String sql = "SELECT * FROM hhs.GitPractice where LabName = \"" + item+ "\"";
+	        rs = st.executeQuery(sql);
+	        
+	        rs.last();
+	        int NumRow = rs.getRow();
+	        
+	        if (NumRow > 0) {
+	        	return true;
+	        } else {
+	        	return false;
+	        }
 		}
 	}
 	
-	public static void main(String[] argv) {
+	public static void main(String[] argv) throws SQLException, IOException {
 		DB myDB = new DB();
 		
-		String id = null;
-		String pw = null;
 		String[] items = null;
-		String find_item = null;
+		String find_item = "도로 연구실";
 		
-		myDB.open(id, pw);
+		myDB.open();
 		
 		items = myDB.list();
 		for(String item : items) {
