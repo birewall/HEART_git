0869b3a0093a39a4b041e7e6211192a9c8a32da0
HOONI-PC\hooni <gnstjdok@naver.com>
2018-04-14 16:41:30 +0900

    hooni modified

--- Git_Practice/src/Practice1.java
+++ Git_Practice/src/Practice1.java
@@ -1,38 +1,56 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Practice1 {
 	public static class DB{
 		Connection con;
-		
 		DB(){
 			con = null;
 		}
-		
-		void open(String id, String pw) {
-			/* Fill */
+		void open() throws SQLException, ClassNotFoundException {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://35.201.230.135/hooni?useSSL=false", "javateam", "boradori1");
 		}
-		
-		String[] list() {
+		String[] list() throws SQLException {
 			String item[] = null;
-			/* Fill */
+
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from survey_table");
+			rs.last();
+			int num_item = rs.getRow();
+			rs.beforeFirst();
+			item = new String[num_item];
+			
+			int index = 0;
+			while(rs.next()) {
+				item[index] = rs.getString(1);
+				index++;
+			}
+			
 			return item;
 		}
-		
-		boolean find(String item) {
-			/* Fill */
-			return false;
+		boolean find(String item) throws SQLException {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from survey_table where name="+item);
+			rs.last();
+			int num_item = rs.getRow();
+			rs.beforeFirst();
+			
+			if( num_item > 0 ) return true;
+			else return false;
 		}
 	}
 	
-	public static void main(String[] argv) {
+	public static void main(String[] argv) throws ClassNotFoundException, SQLException {
 		DB myDB = new DB();
 		
-		String id = null;
-		String pw = null;
 		String[] items = null;
-		String find_item = null;
+		String find_item = "1234";
 		
-		myDB.open(id, pw);
+		myDB.open();
 		
 		items = myDB.list();
 		for(String item : items) {
