d82bb74df9d0f610c361ad999f3e171d35a0a583
CAU HHS <CAU HHS@10.210.57.131>
2018-04-21 17:03:10 +0900

    Main

--- Phonebook/src/Main.java
+++ Phonebook/src/Main.java
@@ -1,7 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
-public class Main {
-	public static void main(String[] argv) {
-		
-		
+public class Main extends Application {
+	 @Override
+	 public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("VMainScene.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			} catch(Exception e) {
+				e.printStackTrace();
+		}
+	 }	
+	 
+	public static void Main(String[] args) {
+		launch(args);
 	}
 }
