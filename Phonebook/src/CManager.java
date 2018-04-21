fd9aebb9d1d74014768095793f668dee0d1fc38a
hooni <hooni@HOONI-PC>
2018-04-21 17:12:41 +0900

    수정

--- Phonebook/src/CManager.java
+++ Phonebook/src/CManager.java
@@ -1,10 +1,8 @@
 
 public class CManager {
 	MBookDB database = new MBookDB();
-	CManager manager = new CManager();
 	
 	public CManager(){
 		database = new MBookDB();
-		manager = new CManager();
 	}
 }
