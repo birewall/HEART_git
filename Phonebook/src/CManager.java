1ad2114efa2dbd9ffa870a17c39469ee4b4b15ea
ffree <ffree@10.210.56.145>
2018-04-21 18:09:49 +0900

    asdf

--- Phonebook/src/CManager.java
+++ Phonebook/src/CManager.java
@@ -1,28 +1,36 @@
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class CManager {
-	
+
 	MBookDB database = new MBookDB();
 	
 	public CManager(){
 		database = new MBookDB();
 	}
-	
+
+    @FXML
+    private TableView<?> tblBook;
+
     @FXML
     private Button btnBookReturn;
 
     @FXML
-    private TextField ReturnBookName;
-
-    @FXML
     private Button btnDelete;
-
+    
     @FXML
     private Button btnAdd;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Button btnBookRent;
+    
+    @FXML
+    private TextField ReturnBookName;
 
     @FXML
     private TextField AddBookName;
@@ -32,44 +40,7 @@ public class CManager {
 
     @FXML
     private TextField ReturnBookID;
-
-    @FXML
-    private TableView<?> tblBook;
-
-    @FXML
-    private TextField RentBookName;
-
-    @FXML
-    private TextField DeleteBookName;
-
-    @FXML
-    private Button btnExit;
-
-    @FXML
-    private Button btnBookRent;
-
-    @FXML
-    void OnBookRent(ActionEvent event) {
-
-    }
-
-    @FXML
-    void OnBookReturn(ActionEvent event) {
-
-    }
-
-    @FXML
-    void OnAdd(ActionEvent event) {
-
-    }
-
-    @FXML
-    void OnDelete(ActionEvent event) {
-
-    }
-
-    @FXML
-    void OnExit(ActionEvent event) {
-
-    }
+    
+      //initialize
+    
 }
