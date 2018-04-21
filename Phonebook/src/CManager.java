9976ee82335001ceba79dc9356bc5c16b822771f
CAU HHS <CAU HHS@10.210.57.131>
2018-04-21 17:42:11 +0900

    fxml하고 CManage Skeleton

--- Phonebook/src/CManager.java
+++ Phonebook/src/CManager.java
@@ -1,8 +1,50 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class CManager {
+
 	MBookDB database = new MBookDB();
 	
 	public CManager(){
 		database = new MBookDB();
 	}
+	
+    @FXML
+    private Button btnBookReturn;
+
+    @FXML
+    private TextField ReturnBookName;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private TextField AddBookName;
+
+    @FXML
+    private TextField RentBookID;
+
+    @FXML
+    private TextField ReturnBookID;
+
+    @FXML
+    private TableView<?> tblBook;
+
+    @FXML
+    private TextField RentBookName;
+
+    @FXML
+    private TextField DeleteBookName;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Button btnBookRent;
+
 }
