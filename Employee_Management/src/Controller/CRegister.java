3ec2a537cce82fb0d1ebc645d038e498d81a00cf
CAU HHS <CAU HHS@10.210.56.180>
2018-07-25 14:29:38 +0900

    vdvd

--- Employee_Management/src/Controller/CRegister.java
+++ Employee_Management/src/Controller/CRegister.java
@@ -1,6 +1,7 @@
 package Controller;
 import java.io.File;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import Model.MComboBoxAutoComplete;
@@ -16,6 +17,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
@@ -24,7 +26,7 @@ import javafx.stage.Stage;
 public class CRegister implements Initializable {
 	MEmployee new_employee;
 	CList parent_controller;
-	String image_path;
+	String image_path = null;
 	MComboBoxAutoComplete<String> autocmb;
 	
     @FXML
@@ -69,16 +71,39 @@ public class CRegister implements Initializable {
 		/* Apply to view */
 		this.cmbDepart.getItems().add(dialog.getEditor().getText());
     }
-
-    @FXML
-    void OnDone(ActionEvent event) {
-    	/* Fill */
-    }
-
+    
+    
+	File selectedFile = null;
     @FXML
     void OnRegisterImage(ActionEvent event) {
-    	/* Fill */
-	        
+	    /* Choose file */
+		FileChooser fc = new FileChooser();
+		fc.setInitialDirectory(new File("."));
+		selectedFile = fc.showOpenDialog(null);    
+		
+		/*Set image*/
+
+		Image image = new Image(selectedFile.toURI().toString());
+		this.imvFace.setImage(image);	
+    }
+    
+    @FXML
+    void OnDone(ActionEvent event) {
+    	/*Save Name*/
+    	String new_txtName = this.txtName.toString();
+    	this.new_employee.setName(new_txtName);
+    	/*Save Date*/
+    	String new_txtDate = this.txtDate.toString();
+    	this.new_employee.setEnroll_date(new_txtDate);
+    	/*Save Department*/
+    	String new_txtDepartment = this.cmbDepart.toString();
+    	this.new_employee.setDepartment(new_txtDepartment);
+    	/*Save Image View Path*/
+		image_path = selectedFile.getPath().toString();
+		this.new_employee.setImage_path(image_path);
+		
+		/*Send to CList*/
+		
     }
 
     void setPerentController(CList controller) {
