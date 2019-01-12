ffe7274424aacf6206a163ecb1e82c1db82e9580
CAU HHS <CAU HHS@10.210.56.233>
2019-01-12 16:21:42 +0900

    sada

---
+++ HEART_Butterfly/src/Controller/CSearchLocManagement.java
@@ -0,0 +1,46 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class CSearchLocManagement extends AbsMetaController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private ListView<?> ListSection;
+
+    @FXML
+    private Button btnInsert;
+
+    @FXML
+    private Button btnDone;
+
+    @FXML
+    private TextField txtMaxSection;
+
+    @FXML
+    void OnInsert(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnSectionTyping(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnDone(ActionEvent event) {
+
+    }
+
+}
